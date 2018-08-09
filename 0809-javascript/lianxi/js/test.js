e70342993dac584e1853696d5ed2b1db1dcfd050
j415 <1720049083@qq.com>
2018-08-09 21:59:13 +0800

    add lianxi js

---
+++ 0809-javascript/lianxi/js/test.js
@@ -0,0 +1,76 @@
+//console.log("------------------------------0-")
+//
+//var d1 = Date()
+//console.log(d1)
+//
+////字符串类型
+//var d = new Date("2013-01-04")    
+//console.log(d)
+//
+////对象类型
+//var  d = new Date(2015,1,1,12,23,34)
+//console.log(d)
+//
+//var  d = new Date()
+//d.setFullYear(2008)
+//console.log(d)
+
+//console.log("------------------------------1-")
+//
+//var d = new Date()
+//
+//var str = d.toLocaleString()
+//console.log(str)
+//var str1 = d.toLocaleTimeString()
+//console.log(str1)
+//var str2 = d.toLocaleDateString()
+//console.log(str2)
+
+//console.log("------------------------------2-")
+//
+//var d1 = new Date()
+//
+//var d2 = new Date(2016, 10 ,11)
+//
+//console.log(d1 - d2)
+//
+////console.log(d1 + d2)   无意义
+
+//console.log("------------------------------3-")
+//
+//console.log(window.document);
+//console.log(window.frames);			//是当前页面中所有框架的集合
+//console.log(window.navigator);   	//反映浏览器及其功能信息的对象
+//console.log(window.screen);			//提供浏览器以外的环境信息
+//console.log(window.location);
+//console.log(window.history);
+//
+//console.log("------------------------------4-")
+
+//function func(){
+//	window.location.href="red.html"
+//	window.location.reload(true)
+//	window.location.assign("red.html")
+//}
+
+function func1(){
+	window.open('red.html', "blank", "width = 400px, height=400px, left=50px,top=0")
+}
+function func2(){
+	window.close()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
